54b98308bac25dbcc997c17b7e299d7151306294
Michael Nock <michael.nock@daxko.com>
2023-08-31 15:10:03 -0400

    Add splash screen

--- app/src/main/java/mi/xi/timer/MainActivity.kt
+++ app/src/main/java/mi/xi/timer/MainActivity.kt
@@ -13,9 +13,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import dagger.hilt.android.AndroidEntryPoint
 import mi.xi.timer.data.UserManager
@@ -24,6 +28,7 @@ import mi.xi.timer.ui.screens.LoginScreen
 import mi.xi.timer.ui.screens.home.homeGraph
 import mi.xi.timer.ui.screens.login.loginGraph
 import mi.xi.timer.ui.screens.signup.signupGraph
+import mi.xi.timer.ui.screens.splash.SplashScreen
 import mi.xi.timer.ui.theme.TimerTheme
 import javax.inject.Inject
 
@@ -37,10 +42,17 @@ class MainActivity : ComponentActivity() {
         setContent {
             val navController = rememberAnimatedNavController()
             val currentUser by userManager.currentUser.collectAsState()
+            var userCheckComplete by remember { mutableStateOf(false) }
 
-            LaunchedEffect(Unit) { userManager.checkUserLoggedIn() }
+            LaunchedEffect(Unit) {
+                userManager.checkUserLoggedIn()
+                userCheckComplete = true
+            }
 
-            val start = if (currentUser == null) LoginScreen.route else HomeScreen.route
+            val start = if (!userCheckComplete) "splash"
+            else if (currentUser != null) HomeScreen.route
+            else LoginScreen.route
+
             TimerTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -59,6 +71,7 @@ fun MainNavHost(
     navController: NavHostController
 ) {
     AnimatedNavHost(navController = navController, startDestination = start) {
+        composable("splash") { SplashScreen() }
         loginGraph(navController = navController)
         signupGraph(navController = navController)
         homeGraph(navController = navController)
---
+++ app/src/main/java/mi/xi/timer/ui/screens/splash/SplashScreen.kt
@@ -0,0 +1,12 @@
+package mi.xi.timer.ui.screens.splash
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import mi.xi.timer.ui.components.CenterColumn
+
+@Composable
+fun SplashScreen() {
+    CenterColumn {
+        Text(text = "TaskTracker v1.0")
+    }
+}
